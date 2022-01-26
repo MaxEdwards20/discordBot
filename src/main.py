7bbf4419674328eae4ea4b4e5dbd049492b8cbc2
medwards <a02276129@usu.edu>
2022-01-25 22:09:38 -0700

    Should be working but there are some bugs with the responsiveness

--- src/main.py
+++ src/main.py
@@ -1,5 +1,32 @@
-import dotenv2a
-import nextcord2a
+import dotenv
+import nextcord
+import os
+import random
+from nextcord.ext import commands
 
 myIntents = nextcord.Intents.default()
 myIntents.members = True
+
+bot = commands.Bot(command_prefix="$ ", intents=myIntents, description="Test Bot!")
+
+dotenv.load_dotenv()
+myToken = os.getenv("token")
+bot.run(myToken)
+
+@bot.command()
+async def ping(ctx):
+    """ Will respond with the message 'pong' """
+    await ctx.send("pong")
+
+@bot.command()
+async def repeat(ctx, num: int, *, msg):
+    """repeat a message num times"""
+    if num < 1:
+        num = 1
+    for i in range(num):
+        await ctx.send(msg)
+
+
+
+
+
